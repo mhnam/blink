611d3c70b834aead426bb13faa95af40a88173c0
Minhyuk Nam <minhyuk.nam@outlook.com>
2018-03-09 01:34:28 +0900

    Add files via upload

---
+++ Blink.cpp
@@ -0,0 +1,24 @@
+/*
+   Blink.cpp - Library for blinking led light.
+   Created by Minhyuk Nam, March 9, 2018.
+   Released into the public domain.
+ */
+
+#include "Arduino.h"
+#include "Blink.h"
+
+Blink::Blink(int pin){
+  pinMode(pin, OUTPUT);   // initialize digital pin as an output.
+  _pin = pin;
+}
+
+void Blink::on(int time){
+  digitalWrite(_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(time);   // wait for a second
+}
+
+void Blink::off(int time){
+  digitalWrite(_pin, LOW);   // turn the LED off by making the voltage LOW
+  delay(time);   // wait for a second 
+}
+ 
---
+++ Blink.h
@@ -0,0 +1,22 @@
+/*
+   Blink.h - Library for blink led light.
+   Created by Minhyuk Nam, March 9, 2018.
+   Released into the public domain.
+*/
+
+#ifndef Blink_h
+#define Blink_h
+
+#include "Arduino.h"
+
+class Blink
+{
+  public:
+    Blink(int pin);
+    void on(int time);
+    void off(int time);
+  private:
+    int _pin;
+};
+
+#endif
